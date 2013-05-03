2e5226de1665e2da277290e9d9eb92cfb3aecaa5
navynine <navynine@gmail.com>
2013-05-03 17:06:55 +0400

    Several test files are created

---
+++ Java/crimsonline/src/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: user
+ * Date: 03.05.13
+ * Time: 16:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static String getHelloMessage() {
+        return "Hello world! Let's code now!";
+    }
+}
